7d520c95997fdea3df4c16315b46f33d912fed0b
Andy Chen <amazingandyyy@gmail.com>
2022-04-17 01:14:42 +0100

    docs: add readme and install script

---
+++ README.md
@@ -0,0 +1,24 @@
+# goapprove
+
+Go approve a PR.
+
+# Installation
+
+```sh
+bash <(curl -sL https://raw.githubusercontent.com/amazingandyyy/goapprove/main/install.sh)
+```
+
+## Preparation
+
+- [Generate a Github personal access token](https://github.com/settings/tokens/new) with [repo] scope
+
+
+## Usage
+
+```
+goapprove -url https://github.com/amazingandyyy/go-approve/pull/1
+```
+
+## LICENSE
+
+MIT
--- install.sh
+++ install.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 
+default_version=0.0.1
 export version=0.0.1
+
+if [ -n "$1" ]; then
+  version=$1
+fi
+
+echo "> installing goapprove@$version"
 curl -LsO https://github.com/amazingandyyy/goapprove/archive/refs/tags/$version.zip &&
 unzip -o $version.zip &&
 sudo touch /opt/homebrew/bin/goapprove &&
 mv -f goapprove-$version/bin/goapprove /opt/homebrew/bin &&
 rm -rf goapprove-$version $version.zip
 
-echo install goapprove@$version success! try 'goapprove --url'
+echo "> install goapprove@$version success! try 'goapprove --url'"
