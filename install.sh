e86c5ef2c3e4c00eb51066fb7865b785e62495e2
Andy Chen <amazingandyyy@gmail.com>
2022-04-17 12:54:20 +0100

    docs: new install script

--- install.sh
+++ install.sh
@@ -1,17 +1,27 @@
 #!/usr/bin/env bash
 
-default_version=0.0.1
-export version=0.0.1
+export version=0.0.2
 
 if [ -n "$1" ]; then
   version=$1
 fi
 
+set -x
+
 echo "> installing goapprove@$version"
 curl -LsO https://github.com/amazingandyyy/goapprove/archive/refs/tags/$version.zip &&
 unzip -o $version.zip &&
+rm -rf /opt/homebrew/bin/goapprove &&
 sudo touch /opt/homebrew/bin/goapprove &&
-mv -f goapprove-$version/bin/goapprove /opt/homebrew/bin &&
-rm -rf goapprove-$version $version.zip
+mv -f goapprove-$version/bin/goapprove /opt/homebrew/bin
+# rm -rf goapprove-$version goapprove-$version.zip
 
-echo "> install goapprove@$version success! try 'goapprove -url <PR URL>'"
+if ! [ -x "$(command -v goapprove)" ]; then
+  echo 'Error: goapprove failed to install' >&2
+  exit 1
+else
+  echo "> install goapprove@$version successfully!"
+  goapprove -help
+fi
+
+set +x
--- scripts/release.sh
+++ scripts/release.sh
@@ -2,4 +2,8 @@
 
 REPO_DIR="$(dirname "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )")"
 
+set -x
+
 go build goapprove.go && mv goapprove $REPO_DIR/bin/goapprove
+
+set +x
